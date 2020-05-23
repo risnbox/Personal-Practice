9c344d836e44addfa73a79d61537694c8b2ce801
risnfox <risnfox@gmail.com>
2020-05-23 18:54:31 +0800

    Revert "Bug"
    This reverts commit 94b3bbc5b7da28d946c4351c5e72661e93173d1c.

--- Asp.net_Exercise/Asp.net_Exercise/Models/JsonIgnore.cs
+++ Asp.net_Exercise/Asp.net_Exercise/Models/JsonIgnore.cs
@@ -22,18 +22,4 @@ namespace Asp.net_Exercise.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Member_Store> Member_Store { get; set; }
     }
-    [MetadataType(typeof(P_C_TMeta))]
-    public partial class Prod_Class_Type
-    {
-
-    }
-    public partial class P_C_TMeta
-    {
-        [JsonIgnore]
-        public virtual Class Class { get; set; }
-        [JsonIgnore]
-        public virtual Type Type { get; set; }
-        [JsonIgnore]
-        public virtual Product Product { get; set; }
-    }
 }
