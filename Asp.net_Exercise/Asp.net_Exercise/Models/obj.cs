94e946c4222dea478d130a0ffb755fb78aee6ddd
risnbox <63708110+risnbox@users.noreply.github.com>
2020-10-27 20:55:35 +0800

    還原

---
+++ Asp.net_Exercise/Asp.net_Exercise/Models/obj.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Asp.net_Exercise.Models
+{
+    public class obj
+    {
+        
+        public class prod_img
+        {
+            public Product prod { get; set; }
+            public Img img { get; set; }
+            public string keep { get; set; }
+            public void Addd(Product P,Img M)
+            {
+                prod = P;
+                img = M;
+            }
+        }
+        public class Paydata
+        {
+            //參數詳情請至綠界API文件https://www.ecpay.com.tw/Content/files/ecpay_011.pdf
+            public string MerchantID = "2000214";//6
+            public string MerchantTradeNo { get; set; }//8
+            public string MerchantTradeDate = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");//7
+            public string PaymentType = "aio";//9
+            public string TotalAmount { get; set; }//11
+            public string TradeDesc = "金流測試";//12
+            public string ItemName { get; set; }//5
+            public string ReturnURL = "https://asptest.ml:45678/cart/VerifyPay";//10
+            public string ChoosePayment = "Credit";//2
+            public string CheckMacValue { get; set; }//1
+            public string ClientBackURL = "https://asptest.ml:45678/members/orderview";//3
+            public string EncryptType = "1";//4
+            public void CreateCheckMacValue(obj.Paydata Pay)
+            {
+                string body = Pay.ToString();
+                string data = string.Format("HashKey=5294y06JbISpM5x9&{0}&HashIV=v77hoKGq4kWxNNIS", body);
+                data = HttpUtility.UrlEncode(data).ToLower();
+                byte[] result = Encoding.Default.GetBytes(data);
+                using (SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider())
+                {
+                    result = sha.ComputeHash(result);
+                }
+                string str = "";
+                for(int i = 0; i < result.Length; i++)
+                {
+                    str += result[i].ToString("X2");
+                }
+                CheckMacValue = str.ToUpper();
+            }
+            public override string ToString()
+            {
+                string str = "ChoosePayment=" + ChoosePayment + "&ClientBackURL="+ ClientBackURL + "&EncryptType=" + EncryptType + "&ItemName=" + ItemName + "&MerchantID=" + MerchantID +
+                    "&MerchantTradeDate=" + MerchantTradeDate + "&MerchantTradeNo=" + MerchantTradeNo + "&PaymentType=" + PaymentType + "&ReturnURL=" +
+                    ReturnURL + "&TotalAmount=" + TotalAmount + "&TradeDesc=" + TradeDesc;
+                return str;
+            }
+            public string ToUrl()
+            {
+                string str = "ChoosePayment=" + ChoosePayment + "&ClientBackURL=" + ClientBackURL + "&EncryptType=" + EncryptType + "&ItemName=" + ItemName + "&MerchantID=" + MerchantID +
+                    "&MerchantTradeDate=" + MerchantTradeDate + "&MerchantTradeNo=" + MerchantTradeNo + "&PaymentType=" + PaymentType + "&ReturnURL=" +
+                    ReturnURL + "&TotalAmount=" + TotalAmount + "&TradeDesc=" + TradeDesc + "&CheckMacValue=" + CheckMacValue;
+                return str;
+            }
+        }
+    }
+}
---
+++ Asp.net_Exercise/Asp.net_Exercise/Scripts/Keep.js
@@ -0,0 +1,24 @@
+﻿function DelKeep(Pid) {
+    var c = confirm("確定移除該收藏嗎?");
+    if (c) {
+        $.ajax({
+            url: "/api/membersapi/DelKeep?Pid=" + Pid,
+            success: function (data) {
+                $("#Father li").remove();
+            }
+        })
+    }
+}
+$(function () {
+
+    for (var i = 0; L > i; i++) {
+        $('#Father').append(
+            "<li class='prod' id='" + data[i].prod.Id + "'>" +
+            "<img src='/Img/btn_del.png' class='delbtn' onclick='DelKeep(" + data[i].prod.Id + ")' id='" + data[i].prod.Id + "'/>" +
+            "<a href='/home/ProdDetails?Pid=" + data[i].prod.Id + "'><img src='/UpdataFiles/" + data[i].img.FileName + "' class='img' /></a>" +
+            "<p class='name'>" + data[i].prod.Name + "</p>" +
+            "<p class='price'>NT$" + data[i].prod.Price + "</p>" +
+            "</li>"
+        )
+    }
+})
